49f71eefaaeaf5ba14c241f8796e1d36f440b81c
Leticia Aldaco <leticiaaldaco@leticias-mbp.lan>
2021-08-22 16:52:04 -0500

    trouble shoot displaying questions

--- assets/script.js
+++ assets/script.js
@@ -2,9 +2,9 @@ var questions = document.getElementById("questions");
 var timer = document.getElementById("time");
 var scoreText = document.getElementById("score");
 var startBtn = document.getElementById("startBtn");
-var greetings = document.querySelector("greetings");
+var greetings = document.querySelector(".greetings");
 var textQuestion = document.getElementById("textQuestion");
-var quizQuestions = document.querySelector("quizQuestions");
+var quizQuestions = document.querySelector(".quizQuestions");
 var userInitials = document.getElementById("userInitials");
 
 //Answers
@@ -18,18 +18,16 @@ var timeLeft = 60;
 
 //Display timer and start countdown when button pressed
 function startQuiz() {
-  greeting.style.display = "none";
+  greetings.style.display = "none";
   timer.style.textContent = "Time: 60";
   console.log("hello");
-  cXZzx xvhnk'o'
-  ;
-  :untdown();
+  countdown();
   quizQuestions.style.display = "flex";
 }
 
 // Timer countdown
 function countdown() {
-  var timerInternal = setInterval(function () {
+  var timerInterval = setInterval(function () {
     if (timeLeft > 1) {
       timer.textContent = timeLeft + "seconds";
       timeLeft--;
@@ -45,56 +43,56 @@ function countdown() {
   showQuestions();
 }
 
-// Question Loop 
+// Question Loop
 var questions = [
-    {
-        title: "What language is used to create web pages?",
-        choices: ["HTML", "CSS", "JavaScript", "All of the above"],
-        answer: "All of the above"
-    },
-    {
-        title: "Which of the following is the largest?",
-        choices: ["H1", "H2", "H3", "H4"],
-        answer: "H1"
-    },
-    {
-        title: "Which langage is the most commonly used to style a page?",
-        choices: ["MongoDB", "GraphQL", "CSS", "MySQL"],
-        answer: "CSS"
-    },
+  {
+    title: "What language is used to create web pages?",
+    choices: ["HTML", "CSS", "JavaScript", "All of the above"],
+    answer: "All of the above",
+  },
+  {
+    title: "Which of the following is the largest?",
+    choices: ["H1", "H2", "H3", "H4"],
+    answer: "H1",
+  },
+  {
+    title: "Which langage is the most commonly used to style a page?",
+    choices: ["MongoDB", "GraphQL", "CSS", "MySQL"],
+    answer: "CSS",
+  },
 ];
 
 function showQuestions() {
-    textQuestion.textContent = questions[nextQuestion].question;
-    button1.textContent =  questions[nextQuestion].answer[0];
-    button2.textContent = questions[nextQuestion].answer[1];
-    button3.textContent = questions[nextQuestion].answer[2];
+  textQuestion.textContent = questions[nextQuestion].title;
+  button1.textContent = questions[nextQuestion].choices[0];
+  button2.textContent = questions[nextQuestion].choices[1];
+  button3.textContent = questions[nextQuestion].choices[2];
+  button4.textContent = questions[nextQuestion].choices[3];
 }
 
-var prevQuestion = questions.length -1;
-var nextQuestion = 0; 
+var prevQuestion = questions.length - 1;
+var nextQuestion = 0;
 var score = 0;
 
-function checkAnswer(buttonNum){
-    console.log(buttonNum);
+function checkAnswer(buttonNum) {
+  console.log(buttonNum);
 
-    if (
-        questions[nextQuestion].answer[buttonNum -1] ==
-        questions[nextQuestion].correctAnswer
-    ){
-        score++;
-        alert("BINGO!")
-    } else{
-        alert("NOPE!");
-        timeLeft -20;
-    }
-    if (nextQuestion < prevQuestion) {
-        nextQuestion++;
-        showQuestions();
-    } else {
-        timer.style.display = "none";
-    }
-
+  if (
+    questions[nextQuestion].choices[buttonNum - 1] ==
+    questions[nextQuestion].answer
+  ) {
+    score++;
+    alert("BINGO!");
+  } else {
+    alert("NOPE!");
+    timeLeft -= 20;
+  }
+  if (nextQuestion < prevQuestion) {
+    nextQuestion++;
+    showQuestions();
+  } else {
+    timer.style.display = "none";
+  }
 }
 
 // Go Back
@@ -102,25 +100,25 @@ function checkAnswer(buttonNum){
 var back = document.getElementById("back");
 back.addEventListener("click", goBack);
 function goBack() {
-    location.reload();
-    return false;
+  location.reload();
+  return false;
 }
-
 
 // User initials
 
-var initialsInput = document.getElementById("inputName");
+var initialsInput = document.getElementById("userInitials");
 var initialsList = document.querySelector("showScore");
-var submit = document.getElementById("initialsButton");
 
 // High Scores
+var submitBtn = document.getElementById("initialButton");
 
-submit.addEventListener("click", function () {
-    localStorage.setItem("initialsInput", initialsInput.value);
-    highScores();
-});
+function submit() {
+  localStorage.setItem("name", initialsInput.value);
+  highScores();
+}
 
 // Scoreboard
 function highScores() {}
 
 startBtn.addEventListener("click", startQuiz);
+submitBtn.addEventListener("click", submit);
